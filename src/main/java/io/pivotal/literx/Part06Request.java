50975ca046fb941cbf4dd1cd04b842541e8984e1
Ruben Rafael <ruben.rafaelmartinez@theapsgroup.com>
2020-06-12 11:12:08 +0200

    added my responses to Part06Request.java

--- src/main/java/io/pivotal/literx/Part06Request.java
+++ src/main/java/io/pivotal/literx/Part06Request.java
@@ -3,6 +3,7 @@ package io.pivotal.literx;
 import io.pivotal.literx.domain.User;
 import io.pivotal.literx.repository.ReactiveRepository;
 import io.pivotal.literx.repository.ReactiveUserRepository;
+import org.assertj.core.api.Assertions;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
@@ -13,34 +14,43 @@ import reactor.test.StepVerifier;
  */
 public class Part06Request {
 
-	ReactiveRepository<User> repository = new ReactiveUserRepository();
+    ReactiveRepository<User> repository = new ReactiveUserRepository();
 
 //========================================================================================
 
-	// TODO Create a StepVerifier that initially requests all values and expect 4 values to be received
-	StepVerifier requestAllExpectFour(Flux<User> flux) {
-		return null;
-	}
+    // TODO Create a StepVerifier that initially requests all values and expect 4 values to be received
+    StepVerifier requestAllExpectFour(Flux<User> flux) {
+        return StepVerifier.create(flux)
+                .expectNextCount(4)
+                .expectComplete();
+    }
 
 //========================================================================================
 
-	// TODO Create a StepVerifier that initially requests 1 value and expects User.SKYLER then requests another value and expects User.JESSE then stops verifying by cancelling the source
-	StepVerifier requestOneExpectSkylerThenRequestOneExpectJesse(Flux<User> flux) {
-		return null;
-	}
+    // TODO Create a StepVerifier that initially requests 1 value and expects User.SKYLER then requests another value and expects User.JESSE then stops verifying by cancelling the source
+    StepVerifier requestOneExpectSkylerThenRequestOneExpectJesse(Flux<User> flux) {
+        return StepVerifier.create(flux)
+                .expectNextMatches(user -> User.SKYLER.equals(user))
+                .assertNext(user -> Assertions.assertThat(User.JESSE.equals(user)))
+                .thenCancel();
+    }
 
 //========================================================================================
 
-	// TODO Return a Flux with all users stored in the repository that prints automatically logs for all Reactive Streams signals
-	Flux<User> fluxWithLog() {
-		return null;
-	}
+    // TODO Return a Flux with all users stored in the repository that prints automatically logs for all Reactive Streams signals
+    Flux<User> fluxWithLog() {
+        return repository.findAll().log();
+    }
 
 //========================================================================================
 
-	// TODO Return a Flux with all users stored in the repository that prints "Starring:" on subscribe, "firstname lastname" for all values and "The end!" on complete
-	Flux<User> fluxWithDoOnPrintln() {
-		return null;
-	}
+    // TODO Return a Flux with all users stored in the repository that prints "Starring:" on subscribe, "firstname lastname" for all values and "The end!" on complete
+    Flux<User> fluxWithDoOnPrintln() {
+        return repository
+                .findAll()
+                .doOnSubscribe(s -> System.out.println("Starring:"))
+                .doOnNext(p -> System.out.println(p.getFirstname() + " " + p.getLastname()))
+                .doOnComplete(() -> System.out.println("The end!")); // TO BE REMOVED
+    }
 
 }
